cf9e3dafba05ad615aa27bec06952198d404ac19
rimanshupatel <rimanshupatel1@gmail.com>
2025-06-05 20:31:20 +0530

    project deploy

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Metrix Technologies</title>
+
+</head>
+<body>
+   <Header>Metrix Technologies Pvt Ltd</Header>
+   <div class="container">
+    <h2>Welcome to Metrix Technologies Pvt Ltd</h2>
+    <br>
+    <p>
+    Parent Company of Gametosa Esports & Gaming</p><br>
+    
+    <h3>About</h3>
+    <br>
+    <img src="https://gametosa.com/gametosa.png" alt="logo">
+    <h3>Gametosa Esports & Gaming</h3>
+    <p>
+
+Gametosa Esports & Gaming is the ultimate platform for gamers, offering tournament hosting, an in-game marketplace, and a thriving gaming community. We connect players, publishers, and brands to create an immersive and competitive gaming experience.</p>
+
+<a href="https://play.google.com/store/apps/details?id=gametosa.com" class="btn">Download Gametosa App</a>
+<br>
+<a href="https://gametosa.com/" class="btn">Visit Website</a>
+<br>
+<br>
+<h3>Meet Our Director
+</h3>
+<br>
+<div class="team">
+    <div class="member">
+        <img src="https://media.licdn.com/dms/image/v2/D5635AQHozDC2UqCY6w/profile-framedphoto-shrink_200_200/B56ZUsMnPOHQAY-/0/1740203259197?e=1744394400&v=beta&t=VyNBMu0LFOC3AsN1UcNVYCK0-SsIpdriyLcgKO-cFHs" alt="Deep Chatterjee">
+        <h4>Deep Chatterjee</h4>
+        <p>Co-founder & CEO</p>
+    </div>
+    <div class="member">
+        <img src="https://media.licdn.com/dms/image/v2/D5603AQFCZDDyTxa33A/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1722698113865?e=1749081600&v=beta&t=WIp_H1tN3rjF0Jtxh7jB_5tIe490fiikqdzBTNgYreo" alt="">
+        <h4>Aditya Chowdhury</h4>
+        <p>Co-founder & CFO</p>
+    </div>
+    <div class="member">
+        <img src="https://media.licdn.com/dms/image/v2/D5603AQEcGisRmgBvFg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1720962403150?e=1749081600&v=beta&t=l_6ynypNSxlmh53aDcLMgjgR_CnH7LB2hsNFXwEP9H4" alt="">
+        <h4>Raj Mondal</h4>
+        <p>Co-founder & Head of Finance</p>
+    </div>
+</div>
+   </div>
+   <footer>
+        <p>© 2025 Metrix Technologies Pvt Ltd | All Rights Reserved</p>
+    </footer>
+</body>
+</html>
